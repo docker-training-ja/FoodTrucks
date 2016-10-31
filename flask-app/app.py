ce2290909050d0045f6f115905f99d78701458f0
HIGUCHI Daisuke <d-higuchi@creationline.com>
2016-10-31 12:51:45 +0900

    let timeout longer

--- flask-app/app.py
+++ flask-app/app.py
@@ -18,7 +18,7 @@ def load_data_in_es():
         res = es.index(index="sfdata", doc_type="truck", id=id, body=truck)
     print "Total trucks loaded: ", len(data)
 
-def safe_check_index(index, retry=3):
+def safe_check_index(index, retry=10):
     """ connect to ES with retry """
     if not retry:
         print "Out of retries. Bailing out..."
@@ -27,8 +27,8 @@ def safe_check_index(index, retry=3):
         status = es.indices.exists(index)
         return status
     except exceptions.ConnectionError as e:
-        print "Unable to connect to ES. Retrying in 5 secs..."
-        time.sleep(5)
+        print "Unable to connect to ES. Retrying in 60 secs..."
+        time.sleep(60)
         safe_check_index(index, retry-1)
 
 
